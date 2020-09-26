dd4f88be5be5bc7f0db396614b0453660df0c6c0
trietch <trietch@linux.home>
2020-09-26 12:57:28 +0200

    bspline controls rendering

--- src/shapes/BSpline.cpp
+++ src/shapes/BSpline.cpp
@@ -2,12 +2,12 @@
 
 #include <iostream>
 
-BSpline::BSpline(unsigned short order, std::vector<float> knots, std::vector<glm::vec3> controls, glm::vec3 position, glm::vec3 rotation, glm::vec3 scale, MainWindow * main_window)
+BSpline::BSpline(unsigned short order, std::vector<float> knots, std::vector<glm::vec3> controls, bool show_controls, glm::vec3 position, glm::vec3 rotation, glm::vec3 scale, MainWindow * main_window)
 		: Shape(
 			position,
 			rotation,
 			scale,
-			geometry(),
+			geometry(order, knots, controls, show_controls),
 			{RND(), RND(), RND()},
 			{
 				"shaders/vert.vert",
@@ -18,31 +18,38 @@ BSpline::BSpline(unsigned short order, std::vector<float> knots, std::vector<glm
 		, _order(order)
 		, _knots(knots)
 		, _controls(controls)
+		, _show_controls(show_controls)
 {
 	set_geometry();
 }
 
-std::tuple<std::vector<GLfloat>, std::vector<GLfloat>, std::vector<GLuint>> BSpline::geometry() {
-		
-	_order = 4;
-	_controls = {
-		{0, 0, 0},
-		{1, 1, 0},
-		{2, -1, 0},
-		{3, 0, 0},
-		{4, 1, 0},
-		{5, -1, 0},
-		{6, 0, 0},
-	};
+std::tuple<std::vector<GLfloat>, std::vector<GLfloat>, std::vector<GLuint>> BSpline::geometry(unsigned short order, std::vector<float> knots, std::vector<glm::vec3> controls, bool show_controls) {
 
+	/*
 	for (unsigned short i = 0; i < _controls.size()+_order+1; ++i) {
 		_knots.push_back(i);
 	}
+	*/
 
 	std::vector<GLfloat> vertex;
 	std::vector<GLfloat> normals;
 	std::vector<GLuint> indices;
 	GLuint ind = 0;
+	
+	if (show_controls) {
+		std::vector<float> test;
+		for (auto p : controls) {
+			test.push_back(p.x);
+			test.push_back(p.y);
+			test.push_back(p.z);
+		}	
+		vertex = test;
+		normals.assign(test.size(), 1);
+		indices.resize(controls.size());
+		std::iota(std::begin(indices), std::end(indices), 0);
+	}
+
+	/*
 	for (float i = _knots[_order-1]; i < _knots[_controls.size()+1]; i += 0.1f) {
 		auto point = eval(i);
 		vertex.push_back(point.x);
@@ -54,7 +61,24 @@ std::tuple<std::vector<GLfloat>, std::vector<GLfloat>, std::vector<GLuint>> BSpl
 		normals.push_back(1);
 		indices.push_back(ind++);
 	}
-
+	*/
+	/*
+	return 
+	{				{
+					0.0f,		0.5f, 		0.0f, 		// Top Center
+					0.5f,		-0.5f, 		0.0f, 		// Bottom Left
+					-0.5f,		-0.5f, 		0.0f, 		// Bottom Right
+				},
+				{
+					1,		1,		1,
+					1,		1,		1,
+					1,		1,		1,
+				},
+				{
+					0, 2, 1, // Triangle
+				},
+	};
+	*/
 	return {vertex, normals, indices};
 }
 
