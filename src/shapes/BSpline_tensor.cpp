748cb0d58f41d49edbdbe0bc82390c4d036702fb
trietch <trietch@gmail.com>
2020-10-01 21:02:16 +0200

    bsplines tensor fixed

--- src/shapes/BSpline_tensor.cpp
+++ src/shapes/BSpline_tensor.cpp
@@ -19,33 +19,21 @@ BSpline_tensor::BSpline_tensor(unsigned short order, std::vector<std::vector<glm
 		, _show_controls(show_controls)
 		, _controls(controls)
 {
-	// TODO c'est degueux et faux
-	//auto knots = uniform_vector(controls.size()+order+1);
-	//_range = std::tuple<float,float>(knots[order-1], knots[controls.size()]);
-
-	//generate_leading_bsplines(controls, order, knots);
-	//generate_generator_bsplines(order, knots);
-	//
-	
 	generate_leading_bsplines(order);
-	// HYPOTHESE :
-	// range de toutes les leading bsplines sont les mêmes
 	generate_generator_bsplines(order);
 
-	// TODO
-	// TODO
-	// TODO
-	// TODO
-	// TODO
-	// TODO
-	// faire un attribut __nb_gene et _nb_point_gene
-	// a la classe tensor
-	//_nb_gene = static_cast<float>(std::round(std::get<1>(_leading_bsplines.front()->range())-std::get<0>(_leading_bsplines.front()->range())))/delta;
 	_nb_gene = _generator_bsplines.size();
-	_nb_point_gene = _nb_gene;
+	// TODO dernier probleme
+	std::cout << "a: " << std::get<0>(_generator_bsplines.front()->range()) << std::endl;
+	std::cout << "b: " << std::get<1>(_generator_bsplines.front()->range()) << std::endl;
+	std::cout << "c: " << _delta << std::endl;
+	_nb_point_gene = std::floor((std::get<1>(_generator_bsplines.front()->range())-std::get<0>(_generator_bsplines.front()->range()))/_delta);
+	if (_nb_point_gene%2 == 1) {
+		_nb_point_gene++;
+	}
+	std::cout << "res: " << _nb_point_gene << std::endl;
 
 	std::cout << "LE DEBUG" << std::endl;
-	std::cout << _nb_gene << std::endl;
 
 	set_vert_norm_indi(geometry());
 	set_geometry();
@@ -56,30 +44,19 @@ void BSpline_tensor::generate_leading_bsplines(unsigned short order) {
 		auto knots = uniform_vector(bs.size()+order+1);
 		_leading_bsplines.push_back(new BSpline(order, knots, bs));
 	}
-
-	// TODO remove release (verification de l'hypothese faite au dessus)
-	for (int i = 0; i < _leading_bsplines.size()-1; ++i) {
-		assert(std::get<0>(_leading_bsplines[i]->range()) == std::get<0>(_leading_bsplines[i+1]->range()));
-		assert(std::get<1>(_leading_bsplines[i]->range()) == std::get<1>(_leading_bsplines[i+1]->range()));
-	}	
 }
 
 void BSpline_tensor::generate_generator_bsplines(unsigned short order) {
-	// TODO C'est ca qu'il faut changer, utiliser le leading_bspline.range();
 	for (float i = std::get<0>(_leading_bsplines.front()->range()); i < std::get<1>(_leading_bsplines.front()->range()); i += _delta) {
 		std::vector<glm::vec3> generator_controls;
 		for (auto lbs : _leading_bsplines) {
 			auto point = lbs->eval(i);
 			generator_controls.push_back(point);
-			//std::cout << glm::to_string(point) << std::endl;
-			//std::cout << std::get<0>(_range) << " " << std::get<1>(_range) << std::endl;
 		}
-		//std::cout << std::endl;
 		auto knots = uniform_vector(generator_controls.size()+order+1);
 		auto gbs = new BSpline(order, knots, generator_controls);
 		_generator_bsplines.push_back(gbs);
 	}
-	std::cout << "################################" << std::endl;
 }
 
 std::tuple<std::vector<GLfloat>, std::vector<GLfloat>, std::vector<GLuint>> BSpline_tensor::geometry() {
@@ -87,9 +64,10 @@ std::tuple<std::vector<GLfloat>, std::vector<GLfloat>, std::vector<GLuint>> BSpl
 	std::vector<GLfloat> vertex;
 	std::vector<GLfloat> normals;
 	std::vector<GLuint> indices;
-	GLuint ind = 0;
 	
 	/* TODO fix ca pour montrer tout les controls des bsplines
+	GLuint ind = 0;
+
 	if (_show_controls) {
 		std::vector<float> flat;
 		for (auto p : _controls) {
@@ -109,7 +87,6 @@ std::tuple<std::vector<GLfloat>, std::vector<GLfloat>, std::vector<GLuint>> BSpl
 		for (float i = std::get<0>(_generator_bsplines.front()->range()); i < std::get<1>(_generator_bsplines.front()->range()); i += _delta) {
 			auto point = gbs->eval(i);
 
-			std::cout << glm::to_string(point) << std::endl;
 			vertex.push_back(point.x);
 			vertex.push_back(point.y);
 			vertex.push_back(point.z);
@@ -117,38 +94,25 @@ std::tuple<std::vector<GLfloat>, std::vector<GLfloat>, std::vector<GLuint>> BSpl
 			normals.push_back(1);
 			normals.push_back(1);
 		}
-		std::cout << std::endl;
 	}
 
 
-	//unsigned long _nb_point_gene = std::floor((std::get<1>(_range)-std::get<0>(_range))/_delta);
-			
-	std::cout << "_nb_gene: " << _nb_gene << " " << _generator_bsplines.size() << std::endl;
+	std::cout << "_nb_gene: " << _nb_gene << std::endl;
 	std::cout << "_nb_point_gene: " << _nb_point_gene << std::endl;
 
 	for (unsigned long i = 0; i < _nb_gene-1; ++i) {
 		for (unsigned long j = 0; j < _nb_point_gene-1; ++j) {
-			unsigned long p = j+i*_nb_gene;
+			unsigned long p = j+i*_nb_point_gene;
 			// First face triangle
 			indices.push_back(p);
-			std::cout << indices.back() << " ";
 			indices.push_back(p+_nb_point_gene);
-			std::cout << indices.back() << " ";
 			indices.push_back(p+1);
-			std::cout << indices.back() << " ";
 
-			std::cout << std::endl;
 			// Second face triangle
 			indices.push_back(p+_nb_point_gene);
-			std::cout << indices.back() << " ";
 			indices.push_back(p+_nb_point_gene+1);
-			std::cout << indices.back() << " ";
 			indices.push_back(p+1);
-			std::cout << indices.back() << " ";
-			std::cout << std::endl;
-			std::cout << std::endl;
 		}
-		std::cout << std::endl;
 	}
 
 	return {vertex, normals, indices};
