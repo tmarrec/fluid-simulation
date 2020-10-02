465f642a3524287010bd672f6dc1437c380bfb6d
trietch <trietch@gmail.com>
2020-10-02 20:03:59 +0200

    reborn start

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,10 @@
 #include "config.h"
-#include "MainWindow.h"
 
 #include <iostream>
-
-#include <QApplication>
-#include <QSurfaceFormat>
 
 int main(int argc, char *argv[])
 {
 	std::cout << "#- " << PROJECT_NAME << " " << PROJECT_VER << " -#" << std::endl << std::endl;
 
-	QApplication a(argc, argv);
-
-	MainWindow w;
-	w.show();
-
-	return a.exec();
+	return EXIT_SUCCESS;
 }
