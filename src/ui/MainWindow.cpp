38fe24f567c1d2658c83c6e4b401f159ae77a4c0
trietch <trietch@linux.home>
2020-10-21 19:02:55 +0200

    fixes control point

--- src/ui/MainWindow.cpp
+++ src/ui/MainWindow.cpp
@@ -8,7 +8,7 @@ MainWindow::MainWindow()
 	format.setVersion(4, 6);
 	format.setProfile(QSurfaceFormat::CoreProfile);
 	format.setDepthBufferSize(24);
-	format.setSamples(8);
+	format.setSamples(16);
 	format.setSwapInterval(0);
 	QSurfaceFormat::setDefaultFormat(format);
 
