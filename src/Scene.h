4ed095cab9eacab01ae2f85231d5cd9e841b8b04
trietch <trietch@linux.home>
2020-11-02 21:21:30 +0100

    gltf get entity vertices

--- src/Scene.h
+++ src/Scene.h
@@ -8,9 +8,7 @@
 #define STB_IMAGE_IMPLEMENTATION
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #define TINYGLTF_USE_CPP14
-#define BUFFER_OFFSET(i) ((char *)NULL + (i))
 
-// #define TINYGLTF_NOEXCEPTION // optional. disable exception handling.
 #include "tinygltf/tiny_gltf.h"
 
 #include "utils.h"
@@ -24,7 +22,7 @@ public:
 		tinygltf::TinyGLTF loader;
 		std::string err;
 		std::string warn;
-		bool ret = loader.LoadASCIIFromFile(&model, &err, &warn, "Box.gltf");
+		bool ret = loader.LoadASCIIFromFile(&model, &err, &warn, "scene.gltf");
 		if (!warn.empty()) {
 			printf("Warn: %s\n", warn.c_str());
 		}
@@ -46,8 +44,7 @@ public:
 
 	}
 
-	void bindModelNodes(std::map<int, GLuint> vbos, tinygltf::Model &model,
-					tinygltf::Node &node)
+	void bindModelNodes(std::map<int, GLuint> vbos, tinygltf::Model &model, tinygltf::Node &node)
 	{
 		if ((node.mesh >= 0) && (node.mesh < model.meshes.size())) {
 			bindMesh(vbos, model, model.meshes[node.mesh]);
@@ -58,33 +55,32 @@ public:
 		}
 	}
 
-	std::map<int, GLuint> bindMesh(std::map<int, GLuint> vbos,
-							   tinygltf::Model &model, tinygltf::Mesh &mesh)
+	std::map<int, GLuint> bindMesh(std::map<int, GLuint> vbos, tinygltf::Model &model, tinygltf::Mesh &mesh)
 	{
 		for (const auto& primitive : mesh.primitives)
 		{
+			std::cout << "new entity" << std::endl;
+			std::vector<float> vertices;
     		for (const auto& attribute : primitive.attributes)
 			{
-        		const tinygltf::Accessor &accessor = model.accessors[attribute.second];
+        		const auto& accessor = model.accessors[attribute.second];
         		if (attribute.first.compare("POSITION") == 0)
 				{
 					// Get vertex buffer
-					tinygltf::BufferView bufferView = model.bufferViews[accessor.bufferView];
-            		tinygltf::Buffer &vertexArray = model.buffers[bufferView.buffer];
+					const auto& bufferView = model.bufferViews[accessor.bufferView];
+            		auto& vertexArray = model.buffers[bufferView.buffer];
 					unsigned char* buffer = &vertexArray.data.at(0) + bufferView.byteOffset + accessor.byteOffset;
 
 					// Cast the vertex buffer to clean float vector
 					float* verticesBuffer = reinterpret_cast<float*>(buffer);
-					ASSERT(accessor.ByteStride(bufferView)/sizeof(float) == 3, "it is not a buffer of float vec3");
-					std::vector<float> vertices {verticesBuffer, verticesBuffer+accessor.count};
-					for (auto v : vertices)
-					{
-						std::cout << v << " ";
-					}
-					std::cout << std::endl;
+					vertices.insert(vertices.cbegin(), verticesBuffer, verticesBuffer+accessor.count);
         		}
-				std::cout << std::endl;
     		}
+			for (auto v : vertices)
+			{
+				std::cout << v << " ";
+			}
+			std::cout << std::endl;
  		} 
 		return vbos;
 	}
