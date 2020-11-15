710551dd79505037a7fda0077391ff2367c57f2e
trietch <trietch@gmail.com>
2020-11-15 14:23:53 +0100

    cleaning and scene for report

--- src/TransformComponent.h
+++ src/TransformComponent.h
@@ -12,6 +12,15 @@ public:
 	, _scale { __scale }
 	{}
 
+	// Testings (frame rate dependent)
+	void update([[maybe_unused]] double __deltaTime) override
+	{
+		if (entity->getEntityID() < 4)
+		{
+			rotate({0.0f, 0.1f, 0.0f});
+		}
+	}
+
 	void move(glm::vec3 __moveVector)
 	{
 		_position += __moveVector;
