ae33cfaddfe81c3d9fe318fb1dfca58fbc89a755
trietch <trietch@linux.home>
2021-04-26 18:55:34 +0200

    Faster Conjugate Gradient (pre-initialized laplacian matrice)

--- src/systems/Fluids.h
+++ src/systems/Fluids.h
@@ -27,19 +27,23 @@ private:
     void Sstep(Fluid3D& fluid);
 
     void addSource(Fluid3D& fluid, std::vector<float>& X, std::vector<float>& S) const;
-    void diffuse(Fluid3D& fluid, std::vector<float>& X, std::vector<float>& Xprev, float diff, std::uint8_t b) const;
+    void diffuse(Fluid3D& fluid, std::vector<float>& X, std::vector<float>& Xprev, std::uint8_t b, Eigen::ConjugateGradient<Eigen::SparseMatrix<double>, Eigen::Lower|Eigen::Upper>& cg);
 
     void advect(Fluid3D& fluid, std::vector<float>& D, std::vector<float>& Dprev, std::vector<float>& X, std::vector<float>& Y, std::vector<float>& Z, std::uint8_t b) const;
-    void project(Fluid3D& fluid, std::vector<float>& X, std::vector<float>& Y, std::vector<float>& Z, std::vector<float>& p, std::vector<float>& div) const;
-    void GaussSeidelRelaxationLinSolve(Fluid3D& fluid, std::vector<float>& X, std::vector<float>& Xprev, float a, float c, std::uint8_t b) const;
+    void project(Fluid3D& fluid, std::vector<float>& X, std::vector<float>& Y, std::vector<float>& Z, std::vector<float>& p, std::vector<float>& div);
 
-    void ConjugateGradientMethodLinSolve(Fluid3D& fluid, std::vector<float>& X, std::vector<float>& Xprev, float a, float c, std::uint8_t b) const;
+    void ConjugateGradientMethodLinSolve(Fluid3D& fluid, std::vector<float>& X, std::vector<float>& Xprev, std::uint8_t b, Eigen::ConjugateGradient<Eigen::SparseMatrix<double>, Eigen::Lower|Eigen::Upper>& cg);
 
     void setBnd(Fluid3D& fluid, std::vector<float>& X, std::uint8_t b) const;
 
     void updateRender(Fluid3D& fluid);
 
     std::shared_ptr<Renderer> _renderer = nullptr;
+
+    bool _initCG = false;
+    Eigen::ConjugateGradient<Eigen::SparseMatrix<double>, Eigen::Lower|Eigen::Upper> _cgProject {};
+    Eigen::ConjugateGradient<Eigen::SparseMatrix<double>, Eigen::Lower|Eigen::Upper> _cgDiffuse {};
+    Eigen::ConjugateGradient<Eigen::SparseMatrix<double>, Eigen::Lower|Eigen::Upper> _cgViscosity {};
 
 #ifdef DEBUG_GUI
     float _debugViscosity;
